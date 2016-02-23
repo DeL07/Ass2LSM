0dced339121e6809486d0a0cb7bb2b9583209218
DeL07 <lscarle07@gmail.com>
2016-02-23 12:35:43 -0500

    work2

--- src/Publication.java
+++ src/Publication.java
@@ -7,7 +7,15 @@ public class Publication {
 	private String publication_authorname; // Name can be split with underscore
 	private double publication_cost;
 	private int publication_nbpages;
-
+	
+	public Publication(long code, String name, int year, String author, double cost, int pages) {
+		publication_code = code;
+		publication_name = name;
+		publication_year = year;
+		publication_authorname = name;
+		publication_cost = cost;
+		publication_nbpages = pages;
+	}
 	
 	
-}
+	}
--- src/PublicationListingProcess1.java
+++ src/PublicationListingProcess1.java
@@ -26,47 +26,98 @@ public class PublicationListingProcess1 extends Publication {
 		PUBLICATIONCOST,
 		PUBLICATIONNBPAGES
 	}
+	
+	int n = 0;
+	Publication[] PublicationArray = new Publication[n];
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		
+		Scanner kb = new Scanner(System.in);
+
 		
 		// Ints
 		
 		
 		// Strings
-		String outputFileName;
+		String outputFileName = null;
 		
 		// Booleans
 		boolean outputFileCheck = false;
+		boolean fileExists = false;
 		
-		Formatter x;
+		File f = null;
+		File originalPublication = new File("PublicationData_Input");
+		PrintWriter pw = null;
+		FileOutputStream outStream = null;
+		FileInputStream inStream = null;
 		
 		System.out.println("Welcome to Mylène and Louis-Simon's publication inventory program");
 		System.out.println("Please enter the name of the output file that will be created to hold the modified/correct inventory");
-		System.out.print("Output file name: ");
-		
-		outputFileName = in.nextLine();
-		
-			try{
-				x = new Formatter(outputFileName);
-				System.out.println("File created: " + outputFileName + ".txt");
-			} catch(IOException e) {
-				System.out.println("you fucked nigg");
+	
+		while (!outputFileCheck) {
+			
+			try {
+				System.out.print("Enter the output filename: ");
+				outputFileName = kb.nextLine();
+				f = new File(outputFileName);
+				fileExists = f.exists();
+				
+				if (fileExists) {
+					System.out.println("Error: There is an existing file called: " + outputFileName + ".");
+					System.out.println("That file already has a size of " + f.length() + " bytes.");
+				} else {
+					f.createNewFile();
+					System.out.println("File created: " + outputFileName + ".txt");
+					outputFileCheck = true;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 			
-			//this all makes no sense
+			Scanner fileIn = null;
 			
-			
-			
-		
-		
+			try {
+				fileIn = new Scanner(new FileReader(originalPublication));
+//				printFileItems(originalPublication);
+			} catch (FileNotFoundException e) {
+				System.out.println("File not found, closing program");
+				e.printStackTrace();
+				
+				System.exit(0);
+			} finally {
+				fileIn.close();
+			}
+
+		}
 	}
 	
-	/*public void correctListOfItems(input file stream, output file stream) {
-		
+//	public static void printFileItems(File inputFile) throws IOException {
+//		Scanner input = new Scanner(new BufferedReader(inputFile));
+//		int x = 0;
+//		while ((x = input.read()) != -1) {
+//			System.out.print((((BufferedReader) input)).readLine());
+//		}
+//	}
+	
+	public static void correctListOfItems(File inFile, File outFile) throws FileNotFoundException, IOException {
+		Reader input = new FileReader(inFile);
+		PrintWriter output = new PrintWriter(outFile);	
 	}
 	
-	public void printFileItems(input file stream name) {
-		
-	}*/
+	public static int numberOfItems(String Filename) throws IOException {
+		LineNumberReader in = new LineNumberReader(new FileReader(Filename));
+		int count = 0;
+		String Line = "";
+		while ((Line = in.readLine()) != null) {
+			
+		}
+		count = in.getLineNumber();
+		in.close();
+		return count;
+	}
+	
+	
 }
+	
+	
+
