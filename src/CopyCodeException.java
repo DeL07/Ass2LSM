6c92f6e6383880552bd24fe79573f49cd424aa8e
Captain-Unicorn <Mylene@Potato.local>
2016-03-06 17:06:20 -0500

    finished exception class
    yay

--- src/CopyCodeException.java
+++ src/CopyCodeException.java
@@ -1,4 +1,35 @@
 
 public class CopyCodeException extends Exception {
 
+	private String message = null;
+	
+	public CopyCodeException(){
+		super();
+	}
+	
+	public CopyCodeException(String message){
+		super(message);
+		this.message = message;
+	}
+	
+	public CopyCodeException(Throwable cause){
+		super(cause);
+	}
+	
+	public CopyCodeException(Throwable cause, String message){
+		super(message, cause);
+		
+	}
+	
+	@Override
+	public String toString(){
+		return message;
+	}
+	
+	@Override 
+	public String getMessage(){
+		return message;
+	}
+	
+	
 }
